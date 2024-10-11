451464e77bfe85e1c5ce1b7e6715f5004f6e7438
Donghyun-Edit <donghyun@editcollective.io>
2024-10-11 23:07:00 +0900

    Add pentagon to example code

--- logic/__init__.py
+++ logic/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import division, absolute_import
 import polyfill  # pylint:disable=C0411
 
-# 이렇게 하위 아이템을 내보낸다고 작성하면
-# 상위 모듈인 `logic`에서 직접 아이템을 불러들일 수 있다.
-# 예를 들어, `from logic import say_hi`가 가능해진다.
-from .sample_module import say_hi
+# 이와 같이 하위 아이템을 내보낼 수 있다.
+# 이를 통해 외부에서 `from logic import say_hi`로
+# 불러들이는 것이 가능해진다.
+from .sample_module import say_hi, draw_pentagon
--- logic/sample_module.py
+++ logic/sample_module.py
@@ -3,6 +3,20 @@
 from __future__ import division, absolute_import
 import polyfill  # pylint:disable=C0411,W0611
 
+import Rhino.Geometry as geo
+
 
 def say_hi():
     print("HI")
+
+
+def draw_pentagon():
+    points = [
+        geo.Point3d(2.5, 0.0, 0.0),
+        geo.Point3d(3.0, 4.0, 0.0),
+        geo.Point3d(0.0, 6.0, 0.0),
+        geo.Point3d(-3.0, 4.0, 0.0),
+        geo.Point3d(-2.5, 0.0, 0.0),
+    ]
+    closing_point = points[0]
+    return geo.PolylineCurve(points + [closing_point])
