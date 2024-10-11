08e74d6df49f903fb4520ee55ed960bfcd855273
Donghyun-Edit <donghyun@editcollective.io>
2024-10-12 01:10:26 +0900

    Rewrite the compatibility comment in English

--- develop/constants.py
+++ develop/constants.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-# 라이노 7에서의 구형 파이썬 2를 지원하기 위한 호환 코드
+# Compatibility code for Python 2 in Rhino 7
 from __future__ import division, absolute_import
 import polyfill  # pylint:disable=C0411,W0611
 
--- develop/module_reloader.py
+++ develop/module_reloader.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-# 라이노 7에서의 구형 파이썬 2를 지원하기 위한 호환 코드
+# Compatibility code for Python 2 in Rhino 7
 from __future__ import division, absolute_import
 import polyfill  # pylint:disable=C0411,W0611
 
@@ -73,7 +73,7 @@ def prepend_startlines(filepath):
     # 이미 잘 추가되어 있다면 신경 쓰지 않는다.
     expected_start_lines = [
         "# -*- coding:utf-8 -*-",
-        "# 라이노 7에서의 구형 파이썬 2를 지원하기 위한 호환 코드",
+        "# Compatibility code for Python 2 in Rhino 7",
         "from __future__ import division, absolute_import",
         (
             "import polyfill  # pylint:disable=C0411"
